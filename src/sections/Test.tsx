baad56faed9c4a3eb4ae8eb212442567aff36b4d
Damián Hernández Huchín <huchin.h.damian@gmail.com>
2023-05-06 22:04:05 -0600

    feat: Add React Native Paper

---
+++ src/sections/Test.tsx
@@ -0,0 +1,13 @@
+import { View } from 'react-native'
+import { Text, Button } from "react-native-paper";
+
+const Test = () => {
+  return (
+    <View>
+      <Text variant='headlineMedium'>Test</Text>
+      <Button mode='contained-tonal'>Hola Mundo</Button>
+    </View>
+  )
+}
+
+export default Test
